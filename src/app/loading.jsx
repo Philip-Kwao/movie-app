5b437935acf0f34a2154ff0789cc41a0fd97a76b
Philip Kwao <kwaoizy@gmail.com>
2023-06-29 01:05:21 +0000

    Added Loading Animated svg

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div className='flex items-center justify-center'>
+        <img src="spin.svg" alt="" />
+    </div>
+  )
+}
+
+export default loading
